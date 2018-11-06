6f0756d8336fb8d9038faeea2427034b493e93b1
SergiiKhylko <aj7000y@gmail.com>
2018-11-06 15:05:02 +0200

    Spring project init commit

---
+++ src/test/java/com/ajkko/ToStuCha/ToStuChaApplicationTests.java
@@ -0,0 +1,16 @@
+package com.ajkko.ToStuCha;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ToStuChaApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
